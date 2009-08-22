715dbb37307fa61eb672a833d779421015db9250
Jacob Rothstein <github@jacobrothstein.com>
2009-08-22 14:55:29 -0700

    geokit api keys

--- config/initializers/geokit_config.rb
+++ config/initializers/geokit_config.rb
@@ -21,12 +21,12 @@ if defined? Geokit
 	# This is your yahoo application key for the Yahoo Geocoder.
 	# See http://developer.yahoo.com/faq/index.html#appid
 	# and http://developer.yahoo.com/maps/rest/V1/geocode.html
-	Geokit::Geocoders::yahoo = 'REPLACE_WITH_YOUR_YAHOO_KEY'
+	Geokit::Geocoders::yahoo = 'fHt9kLbV34Gm3HC8CGyNyzYPjGdvgYtxgAyxA_5toGrzcR56mf0m1BIdGeGFAv3FcBa3H7VY'
     
 	# This is your Google Maps geocoder key. 
 	# See http://www.google.com/apis/maps/signup.html
 	# and http://www.google.com/apis/maps/documentation/#Geocoding_Examples
-	Geokit::Geocoders::google = 'REPLACE_WITH_YOUR_GOOGLE_KEY'
+	Geokit::Geocoders::google = 'ABQIAAAAYGTRQ0tJSljg1ZgaUtAuGBTRuCg5b2pyZ8bsMPW3Vx-89PB5vhSXfu_IjCNYfQGAJ3Ros8WmMt4rOQ'
     
 	# This is your username and password for geocoder.us.
 	# To use the free service, the value can be set to nil or false.  For 
@@ -52,7 +52,7 @@ if defined? Geokit
 	# Be aware that there are Terms of Use restrictions on how you can use the 
 	# various geocoders.  Make sure you read up on relevant Terms of Use for each
 	# geocoder you are going to use.
-	Geokit::Geocoders::provider_order = [:google,:us]
+	Geokit::Geocoders::provider_order = [:google, :yahoo, :us]
 
 	# The IP provider order. Valid symbols are :ip,:geo_plugin.
 	# As before, make sure you read up on relevant Terms of Use for each
