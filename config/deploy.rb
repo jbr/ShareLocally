7265b8fbc1f3ea2b3ad3bdc842781071fd569b36
Jacob Rothstein <github@jacobrothstein.com>
2009-08-21 18:35:31 -0700

    deploy and basic layout

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,29 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "sharelocally"
+set :deploy_to, "/var/www/apps/#{application}"
+default_run_options[:pty] = true
+set :repository,  "git@github.com:railsrumble/rr09-team-237.git"
+set :scm, "git"
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+role :app, "sharelocally.org"
+role :web, "sharelocally.org"
+role :db,  "sharelocally.org", :primary => true
 
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
+namespace :deploy do
+  task :restart do
+    sudo "monit -g sharelocally restart all"
+  end
+  
+  task :start do
+    sudo "monit -g sharelocally start all"
+  end
+  
+  task :stop do
+    sudo "monit -g sharelocally stop all"
+  end
+end
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+namespace :monit do
+  task :status do
+    sudo "monit status"
+  end
+end
