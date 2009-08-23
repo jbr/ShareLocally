5f690f9801101cd81f0c4096a07d26dd6c317ba0
Jacob Rothstein <github@jacobrothstein.com>
2009-08-23 07:07:09 -0700

    reworked replacement_link helper with the help of handy capture_haml

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  
   def replacement_link(text, options = {}, &blk)
-    concat(content_tag('div', {:class => 'replace'}.merge(options)) do
-      link_to(text, '#', :class => 'replacement') +
-        content_tag('div', :style => 'display:none', &blk)
-    end)
+    innermost = link_to(text, '#', :class => 'replacement')
+    innermost += content_tag('div', capture_haml(&blk), :style => 'display:none')
+    concat content_tag('div', innermost, {:class => 'replace'}.merge(options))
   end
 end
