c29c08d380f5c626f82e606f8b1d2818a55f0adf
Jacob Rothstein <github@jacobrothstein.com>
2009-08-23 07:22:48 -0700

    fixed nasty float issue on sign up page

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,8 +1,11 @@
 $(function(){
   setTimeout(function(){ $('#flash div').hide('blind', 500) }, 2500);
   $('.replacement').click(function(){
-    $(this).next('div:not(:visible)').show().end().remove();
+    $(this).next('div:not(:visible)').show().prepend('<a href="#" class="close">x</div>').end().remove();
     return false;
+  })
+  $('.replace .close').live('click', function(){
+    $(this).parent().hide('blind', function(){$(this).remove()})
   })
   $('#search-items .item .searchable').highlightRegex(new RegExp($('#search-items #query').text(), 'gi'));
 })
