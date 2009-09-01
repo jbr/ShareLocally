9d9ce9749f90088975ed4e4698fcd59e7b5b63bc
Jacob Rothstein <github@jacobrothstein.com>
2009-08-31 19:03:17 -0700

    remove comments from production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,23 +5,10 @@ config.action_view.cache_template_loading            = true
 config.log_level = :debug
 config.cache_store = :mem_cache_store
 
-# config.action_controller.asset_host = "http://assets.example.com"
-
 config.action_controller.asset_host =  "http://assets%d.sharelocally.org"
-# Disable delivery errors, bad email addresses will be ignored
-# config.action_mailer.raise_delivery_errors = false
 
-# Enable threaded mode
-# config.threadsafe!
 ActionMailer::Base.delivery_method = :sendmail
 ActionMailer::Base.sendmail_settings = {
   :location       => '/usr/sbin/sendmail',
   :arguments      => '-i -t -f sharelocally@sharelocally.org'
 }
-# 
-# ActionMailer::Base.smtp_settings = {
-#   :domain => 'sharelocally.org',
-#   :user_name => 'sharelocally@sharelocally.org',
-#   :password => 'sharel0cally',
-#   :authentication => 'login'
-# }
